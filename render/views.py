319e0d5e70bc261afa4d83cbe46fe919022333b9
tanbirali <tanbirali2004@gmail.com>
2023-04-27 08:50:53 +0000

    Toast feature added

--- render/views.py
+++ render/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 from .models import Visitor
 from .helper import is_profane
@@ -12,7 +13,9 @@ def index(request):
         if not is_profane(name):
             visitor = Visitor(name=name)
             visitor.save()
-
+            messages.success(request, "Your input has been successfully added.")
+        else:
+            messages.success(request, "You have entered a profane word and it cannot be submitted.")
     return render(request, 'index.html')
 
 
